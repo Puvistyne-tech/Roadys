2aff94e1d791949e6b068d0cdfb1bfd8b16b3ed0
Puvistyne <puvistien@gmail.com>
2022-08-01 16:22:16 +0200

    commit by puvi - Terms and Conditions

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -27,108 +27,110 @@ import SettingsScreen from "./scenes/modal/SettingsScreen";
 import {HeaderTitle} from "@react-navigation/stack";
 import HeaderRight from "./routes/navigation/stacks/HeaderRight";
 import HeaderLeft from "./routes/navigation/stacks/HeaderLeft";
+import TermsAndConditions from "./components/TermsAndConditions/TermsAndConditions";
 
 const MyAppNavigation = () => {
-   const Stack = createNativeStackNavigator();
-   const {token} = useContext(AuthContext);
+    const Stack = createNativeStackNavigator();
+    const {token} = useContext(AuthContext);
 
-   const isAuthenticated = useMemo(
-      () => {
-         console.log("token", token);
-         return token === null
-      }, [token]
-   )
-   console.log("token", token)
+    const isAuthenticated = useMemo(
+        () => {
+            console.log("token", token);
+            return token === null
+        }, [token]
+    )
+    console.log("token", token)
 
-   return <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-         {isAuthenticated ?
-            (
-               <>
-                  <Stack.Screen name="Welcome screen" component={WelcomeScreen}/>
-                  <Stack.Screen name="Sign in" component={SigninScreen}/>
-                  <Stack.Screen name="Sign up" component={SignupScreen}/>
-               </>
-            ) :
-            (
-               <>
-                  <Stack.Screen
-                     name="Root"
-                     component={Drawer}
-                     options={{headerShown: false}}
-                  />
-                  <Stack.Screen
-                     name='PartnerNavigator'
-                     component={PartnerNavigator}
-                     option={{headerShown: true}}
-                  />
-                  <Stack.Screen
-                     name="Feedback"
-                     component={FeedbackScreen}
-                     options={({navigation}) => ({
-                        title: 'Feedback',
-                        headerShown: true,
-                        headerTitle: () => <HeaderTitle/>,
-                        headerRight: () => <HeaderRight/>,
-                     })}
-                  />
-                  <Stack.Screen
-                     name="About Roadys"
-                     component={AboutRoadysScreen}
-                     options={({navigation}) => ({
-                        headerShown: true,
-                        title: 'About Roadys',
-                        headerTitle: () => <HeaderTitle/>,
-                        headerRight: () => <HeaderRight/>,
-                     })}
-                  />
-                  <Stack.Screen
-                     name="Settings"
-                     component={SettingsScreen}
-                     options={({navigation}) => ({
-                        headerShown: true,
-                        title: 'Settings',
-                        headerTitle: () => <HeaderTitle/>,
-                        headerRight: () => <HeaderRight/>,
-                     })}
-                  />
-               </>
-            )
-         }
-      </Stack.Navigator>
-   </NavigationContainer>
+    return <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+            {isAuthenticated ?
+                (
+                    <>
+                        <Stack.Screen name="Welcome screen" component={WelcomeScreen}/>
+                        <Stack.Screen name="Sign in" component={SigninScreen}/>
+                        <Stack.Screen name="Sign up" component={SignupScreen}/>
+                        <Stack.Screen name="TermsAndConditions" component={TermsAndConditions}/>
+                    </>
+                ) :
+                (
+                    <>
+                        <Stack.Screen
+                            name="Root"
+                            component={Drawer}
+                            options={{headerShown: false}}
+                        />
+                        <Stack.Screen
+                            name='PartnerNavigator'
+                            component={PartnerNavigator}
+                            option={{headerShown: true}}
+                        />
+                        <Stack.Screen
+                            name="Feedback"
+                            component={FeedbackScreen}
+                            options={({navigation}) => ({
+                                title: 'Feedback',
+                                headerShown: true,
+                                headerTitle: () => <HeaderTitle/>,
+                                headerRight: () => <HeaderRight/>,
+                            })}
+                        />
+                        <Stack.Screen
+                            name="About Roadys"
+                            component={AboutRoadysScreen}
+                            options={({navigation}) => ({
+                                headerShown: true,
+                                title: 'About Roadys',
+                                headerTitle: () => <HeaderTitle/>,
+                                headerRight: () => <HeaderRight/>,
+                            })}
+                        />
+                        <Stack.Screen
+                            name="Settings"
+                            component={SettingsScreen}
+                            options={({navigation}) => ({
+                                headerShown: true,
+                                title: 'Settings',
+                                headerTitle: () => <HeaderTitle/>,
+                                headerRight: () => <HeaderRight/>,
+                            })}
+                        />
+                    </>
+                )
+            }
+        </Stack.Navigator>
+    </NavigationContainer>
 }
 
 const App = () => {
-   // state
-   const [didLoad, setDidLoad] = useState(false)
+    // state
+    const [didLoad, setDidLoad] = useState(false)
 
-   // handler
-   const handleLoadAssets = async () => {
-      // assets preloading
-      await Promise.all([...imageAssets, ...fontAssets])
-      setDidLoad(true)
-   }
+    // handler
+    const handleLoadAssets = async () => {
+        // assets preloading
+        await Promise.all([...imageAssets, ...fontAssets])
+        setDidLoad(true)
+    }
 
-   // lifecycle
-   useEffect(() => {
-      handleLoadAssets()
-   }, [])
+    // lifecycle
+    useEffect(() => {
+        handleLoadAssets()
+    }, [])
 
-   // rendering
-   if (!didLoad) return <View/>
-   return (
-      <ThemeProvider theme={theme}>
-         <Provider store={store}>
-            <CustomApolloProvider>
-               <AuthProvider>
-                  <MyAppNavigation/>
-               </AuthProvider>
-            </CustomApolloProvider>
-            <FlashMessage/>
-         </Provider>
-      </ThemeProvider>
-   )
+    // rendering
+    if (!didLoad) return <View/>
+    return (
+        <ThemeProvider theme={theme}>
+            <Provider store={store}>
+                <CustomApolloProvider>
+                    <AuthProvider>
+                        <MyAppNavigation/>
+                    </AuthProvider>
+                </CustomApolloProvider>
+                <FlashMessage/>
+            </Provider>
+        </ThemeProvider>
+    )
 }
 
 export default App
